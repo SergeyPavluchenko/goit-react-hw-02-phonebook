3853598fd00b21e61c60b99600df4e6cd67747ae
SergeyPavluchenko <s.pavluchenko90@gmail.com>
2022-12-28 20:39:17 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import { GlobalStyle } from 'components/GlobalStyles';
+import './Global.Styles.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <GlobalStyle/>
   </React.StrictMode>
 );
